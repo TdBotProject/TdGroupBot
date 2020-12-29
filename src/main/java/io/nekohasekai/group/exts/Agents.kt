2e0598f23ad43459474b722e2bc325096da09265
世界 <i@nekox.me>
2020-12-29 17:12:51 +0800

    Fix search self

--- src/main/java/io/nekohasekai/group/exts/Agents.kt
+++ src/main/java/io/nekohasekai/group/exts/Agents.kt
@@ -2,10 +2,12 @@ package io.nekohasekai.group.exts
 
 import io.nekohasekai.ktlib.td.core.TdHandler
 import io.nekohasekai.ktlib.td.core.raw.getChatMember
+import io.nekohasekai.ktlib.td.core.raw.getChatOrNull
 import io.nekohasekai.ktlib.td.extensions.isAdmin
 
 suspend fun TdHandler.isUserAgentAvailable(chatId: Long): Boolean {
     return with(userAgent ?: return false) {
+        getChatOrNull(chatId) ?: return false
         getChatMember(chatId, me.id).isAdmin
     }
 }
